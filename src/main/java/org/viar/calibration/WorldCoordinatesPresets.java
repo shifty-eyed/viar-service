e6027a7eabb7bdf27cbef0f767aacf5836aa6e59
roma <nifect@gmail.com>
2023-04-01 12:57:30 -0400

    presets added

--- src/main/java/org/viar/calibration/WorldCoordinatesPresets.java
+++ src/main/java/org/viar/calibration/WorldCoordinatesPresets.java
@@ -16,6 +16,11 @@ public class WorldCoordinatesPresets {
 	
 	private Map<String, Point2d> xyCoordByPresetName;
 	
+	private static Point2d f2m(double x, double y) {
+		final double multiplier = 0.3048;
+		return new Point2d(x * multiplier, y * multiplier);
+	}
+	
 	@PostConstruct
 	private void init() {
 		zCoordByMarkerId = new HashMap<>();
@@ -26,9 +31,30 @@ public class WorldCoordinatesPresets {
 		zCoordByMarkerId.put(4, 1.98755);
 		
 		xyCoordByPresetName = new HashMap<>();
-		xyCoordByPresetName.put("a1", new Point2d(1,2));
-		xyCoordByPresetName.put("b2", new Point2d(3,4));
-		xyCoordByPresetName.put("b3", new Point2d(5,6));
+		xyCoordByPresetName.put("a: 2,-1", f2m(2, -1));
+		xyCoordByPresetName.put("b: 3,-1", f2m(3, -1));
+		xyCoordByPresetName.put("c: 4,-1", f2m(4, -1));
+		xyCoordByPresetName.put("d: 5,-1", f2m(5, -1));
+		xyCoordByPresetName.put("e: 6,-1", f2m(6, -1));
+		
+		xyCoordByPresetName.put("f: 2,-2", f2m(2, -2));
+		xyCoordByPresetName.put("g: 3,-2", f2m(3, -2));
+		xyCoordByPresetName.put("h: 4,-2", f2m(4, -2));
+		xyCoordByPresetName.put("i: 5,-2", f2m(5, -2));
+		xyCoordByPresetName.put("j: 6,-2", f2m(6, -2));
+		
+		xyCoordByPresetName.put("k: 2,-3", f2m(2, -3));
+		xyCoordByPresetName.put("l: 3,-3", f2m(3, -3));
+		xyCoordByPresetName.put("m: 4,-3", f2m(4, -3));
+		xyCoordByPresetName.put("n: 5,-3", f2m(5, -3));
+		xyCoordByPresetName.put("o: 6,-3", f2m(6, -3));
+		
+		xyCoordByPresetName.put("p: 3,-4", f2m(3, -4));
+		xyCoordByPresetName.put("q: 4,-4", f2m(4, -4));
+		xyCoordByPresetName.put("r: 5,-4", f2m(5, -4));
+		xyCoordByPresetName.put("s: 6,-4", f2m(6, -4));
+		
+		
 	}
 	
 	public Point3d getWorldCoords(String presetName, int markerId) {
